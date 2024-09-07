f665acc5b384298d95501d97bbf3cf56ff868325
nosyos <yoshitaka.nose@gmail.com>
2024-09-07 15:02:24 +0900

    new file:   pextract/pextract.py new fle:   setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+
+setup(
+    name="pextract",
+    version="0.1",
+    description="pextract is a library/CLI tool to extract text from various file formats.",
+    author="",
+    suthor_email="",
+    url="https://github.com/nosyos/pextract"
+)
